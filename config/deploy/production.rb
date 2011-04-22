b98be6f5056b9e153527e0c5e80a05d632168229
James Mead <james@floehopper.org>
2011-04-22 20:57:13 +0100

    Use dyndyn.org domain for now.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ server 'argonaut.slice', :app, :web, :db, :primary => true
 manifest :app, %{
   floehopper::webby_app {'briardene':
     deploy_to => "<%= deploy_to %>",
-    domain => 'briardene.jamesmead.org'
+    domain => 'briardene.homeip.net'
   }
 }
