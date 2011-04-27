72c70f6fcdc6de861db885195834c0abe56594b5
James Mead <james@floehopper.org>
2011-04-27 10:02:44 +0100

    Add proper domain configuration.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ server 'argonaut.slice', :app, :web, :db, :primary => true
 manifest :app, %{
   floehopper::webby_app {'briardene':
     deploy_to => "<%= deploy_to %>",
-    domain => 'briardene.homeip.net'
+    domain => 'durham-house-for-sale.co.uk'
   }
 }
